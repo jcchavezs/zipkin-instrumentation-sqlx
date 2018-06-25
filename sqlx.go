7d8e3a3c917b6e7e62369539ed7148f41a0af542
José Carlos Chávez <jcchavezs@gmail.com>
2018-06-25 13:35:30 +0200

    Improves godoc and adds support for sqlx.Ext and sqlx.ExtContext interfaces.

--- sqlx.go
+++ sqlx.go
@@ -1,3 +1,5 @@
+// SQLx instrumented library with zipkin-go
+
 package zipkinsqlx
 
 import (
@@ -11,11 +13,13 @@ import (
 	"github.com/openzipkin/zipkin-go/reporter"
 )
 
+// DB is a wrapper around sqlx.DB with also required the zipkin tracer
 type DB struct {
 	db *sqlx.DB
 	tr *zipkin.Tracer
 }
 
+// NewDb returns a new sqlx DB wrapper for a pre-existing *sqlx.DB.
 func NewDb(db *sqlx.DB, tr *zipkin.Tracer) *DB {
 	if tr == nil {
 		tr, _ = zipkin.NewTracer(reporter.NewNoopReporter(), zipkin.WithNoopSpan(true))
@@ -29,6 +33,14 @@ func getNameFromQuery(query string) string {
 	return strings.ToLower(re.FindString(query))
 }
 
+// QueryContext executes a query that returns rows, typically a SELECT.
+// The args are for any placeholder parameters in the query.
+func (db *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return db.QueryContext(context.Background(), query, args)
+}
+
+// QueryContext executes a query that returns rows, typically a SELECT.
+// The args are for any placeholder parameters in the query.
 func (db *DB) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
 	s, _ := db.tr.StartSpanFromContext(ctx, getNameFromQuery(query), zipkin.Tags(map[string]string{
 		"sql.query": query,
@@ -43,6 +55,14 @@ func (db *DB) QueryContext(ctx context.Context, query string, args ...interface{
 	return r, err
 }
 
+// Queryx queries the database and returns an *sqlx.Rows.
+// Any placeholder parameters are replaced with supplied args.
+func (db *DB) Queryx(query string, args ...interface{}) (*sqlx.Rows, error) {
+	return db.QueryxContext(context.Background(), query, args)
+}
+
+// QueryxContext queries the database and returns an *sqlx.Rows.
+// Any placeholder parameters are replaced with supplied args.
 func (db *DB) QueryxContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error) {
 	s, _ := db.tr.StartSpanFromContext(ctx, getNameFromQuery(query), zipkin.Tags(map[string]string{
 		"sql.query": query,
@@ -57,6 +77,14 @@ func (db *DB) QueryxContext(ctx context.Context, query string, args ...interface
 	return r, err
 }
 
+// QueryRowx queries the database and returns an *sqlx.Row.
+// Any placeholder parameters are replaced with supplied args.
+func (db *DB) QueryRowx(query string, args ...interface{}) *sqlx.Row {
+	return db.QueryRowxContext(context.Background(), query, args)
+}
+
+// QueryRowxContext queries the database and returns an *sqlx.Row.
+// Any placeholder parameters are replaced with supplied args.
 func (db *DB) QueryRowxContext(ctx context.Context, query string, args ...interface{}) *sqlx.Row {
 	s, _ := db.tr.StartSpanFromContext(ctx, getNameFromQuery(query), zipkin.Tags(map[string]string{
 		"sql.query": query,
@@ -71,6 +99,14 @@ func (db *DB) QueryRowxContext(ctx context.Context, query string, args ...interf
 	return r
 }
 
+// Exec execs a statement in the database and returns an sql.Result.
+// Any placeholder parameters are replaced with supplied args.
+func (db *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return db.ExecContext(context.Background(), query, args)
+}
+
+// ExecContext execs a statement in the database and returns an sql.Result.
+// Any placeholder parameters are replaced with supplied args.
 func (db *DB) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
 	s, _ := db.tr.StartSpanFromContext(ctx, getNameFromQuery(query), zipkin.Tags(map[string]string{
 		"sql.query": query,
@@ -82,9 +118,29 @@ func (db *DB) ExecContext(ctx context.Context, query string, args ...interface{}
 		s.Tag(string(zipkin.TagError), err.Error())
 	}
 
+	if affectedRows, err := r.RowsAffected(); err != nil {
+		s.Tag("db.affected_rows", string(affectedRows))
+	}
+
 	return r, err
 }
 
+// PreparexContext prepares a statement.
+//
+// The provided context is used for the preparation of the statement, not for
+// the execution of the statement.
 func (db *DB) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
 	return db.PrepareContext(ctx, query)
+}
+
+func (db *DB) DriverName() string {
+	return db.db.DriverName()
+}
+
+func (db *DB) Rebind(query string) string {
+	return db.db.Rebind(query)
+}
+
+func (db *DB) BindNamed(query string, arg interface{}) (string, []interface{}, error) {
+	return db.db.BindNamed(query, arg)
 }
--- sqlx_test.go
+++ sqlx_test.go
@@ -1,6 +1,26 @@
 package zipkinsqlx
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/openzipkin/zipkin-go"
+)
+
+func assertExt(_ sqlx.Ext)               {}
+func assertExtContext(_ sqlx.ExtContext) {}
+
+func TestDBReturnsDriverName(t *testing.T) {
+	driverName := "driver"
+	tracer, _ := zipkin.NewTracer(nil)
+	db := NewDb(sqlx.NewDb(nil, driverName), tracer)
+	if want, have := driverName, db.DriverName(); want != have {
+		t.Fatalf("unexpected driver name, expected %s, got %s", want, have)
+	}
+
+	assertExt(db)
+	assertExtContext(db)
+}
 
 func TestGetNameFromQuery(t *testing.T) {
 	queryProvider := [][]string{
@@ -18,4 +38,3 @@ func TestGetNameFromQuery(t *testing.T) {
 		}
 	}
 }
-
